dd533e183f4f55f1899f30b5f05895cbc28dd3e7
PR800 <64070025@it.kmitl.ac.th>
2023-09-16 19:28:53 +0700

    Tutorial outline

--- screens/TutorialScreen.js
+++ screens/TutorialScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 // import AliceCarousel from 'react-alice-carousel';
 
 const TutorialScreen = ({navigation, images}) => {
@@ -15,25 +16,57 @@ const TutorialScreen = ({navigation, images}) => {
 
     return (
         <View style={styles.screen}>
-            <Text>Tutorial Screen !</Text>
-            {/* <AliceCarousel autoPlay autoPlayInterval="3000">
-                <Image 
-                    source={
-                        require('../assets/icon.png')
-                    }
-                />
-                <Image 
-                    source={
-                        require('../assets/icon.png')
-                    }
-                />
-            </AliceCarousel> */}
-            <Text style={{fontSize: 20}}></Text>
-            <Text
-                onPress={() => {
-                    navigation.navigate("homePage", {});
-                }}
-            >x Go to Home (edit navigation here)</Text>
+            <LinearGradient
+                colors={['#FC7D7B', '#9F79EB']}
+                style={styles.gradientBackground}
+            >
+                <View style={styles.top}>
+                    <Text>progress bar</Text>
+                </View>
+
+                <View style={styles.content}>
+                    <Text>tutorial details</Text>
+                    {/* <AliceCarousel autoPlay autoPlayInterval="3000">
+                        <Image 
+                            source={
+                                require('../assets/icon.png')
+                            }
+                        />
+                        <Image 
+                            source={
+                                require('../assets/icon.png')
+                            }
+                        />
+                    </AliceCarousel> */}
+                </View>
+
+                <View style={styles.bottom}>
+                    <TouchableOpacity 
+                        style={styles.button}
+                        // onPress={() => {
+                        //     navigation.navigate("login", {});
+                        // }}
+                    >
+                        <Text style={styles.textButton}>Start tutorial</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        onPress={() => {
+                            navigation.navigate("homePage", {});
+                        }}
+                    >
+                        <Text style={[styles.text, { 
+                            top: 15,
+                            textShadowColor: 'rgba(0, 0, 0, 0.5)',
+                            textShadowOffset: {width: -1, height: 2},
+                            textShadowRadius: 15,
+                        }]}>
+                            Skip for now
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                
+            </LinearGradient>
         </View>
     );
 };
@@ -44,10 +77,64 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    sliderimg: {
+    top: {
+        height: "10%",
         width: "100%",
-        height: "500px",
-    }
+        // backgroundColor: 'pink',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    content: {
+        height: "70%",
+        width: "100%",
+        // backgroundColor: 'grey',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    gradientBackground: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: "white",
+        fontSize: 15,
+        fontWeight: "bold",
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        height: 55,
+        width: 300,
+        borderRadius: 50,
+        justifyContent: 'center',
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 1,
+        elevation: 5,
+    },
+    textButton: {
+        color:"#FF9B80", 
+        fontWeight: "bold",
+        fontSize: 20,
+    },
+    bottom: {
+        height: "20%",
+        width: "100%",
+        // backgroundColor: 'red',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    // sliderimg: {
+    //     width: "100%",
+    //     height: "500px",
+    // }
 });
   
 export default TutorialScreen;
