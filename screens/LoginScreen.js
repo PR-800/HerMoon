1dab6efb67c44b57b76b11d31a4519028e24e892
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-10-23 16:32:13 +0700

    เพิ่ม Dialog

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
 
 import * as Font from 'expo-font';
 
@@ -14,15 +15,15 @@ class LoginScreen extends Component {
         super();
         this.accountCollection = firebase.firestore().collection("accounts");
         this.state = {
-            username: "", 
+            username: "",
             password: "",
-            all_data: [], 
-            showPassword: true, 
-            appIsReady: false, 
+            all_data: [],
+            showPassword: true,
+            appIsReady: false,
             activeUser: null,
         };
     }
-       
+
     inputValueUpdate = (val, prop) => {
         const state = this.state;
         state[prop] = val;
@@ -42,8 +43,8 @@ class LoginScreen extends Component {
     }
 
     componentWillUnmount() {
-        this.unsubscribe();    
-    }   
+        this.unsubscribe();
+    }
 
     prepareResources = async () => {
         await cacheAssets();
@@ -66,63 +67,63 @@ class LoginScreen extends Component {
                 >
                     <View style={styles.header}>
                         <Image
-                            source={ require('../assets/logo.png') }
+                            source={require('../assets/logo.png')}
                         />
                         <View>
                             <Text style={[styles.headerText, {
                                 textShadowColor: '#9F79EB',
-                                textShadowOffset: {width: 1, height: 0.5},
+                                textShadowOffset: { width: 1, height: 0.5 },
                                 textShadowRadius: 35,
                                 marginTop: 15,
                             }]}>Her</Text>
                             <Text style={[styles.headerText, {
                                 textShadowColor: '#9F79EB',
-                                textShadowOffset: {width: 1, height: 0.5},
+                                textShadowOffset: { width: 1, height: 0.5 },
                                 textShadowRadius: 35,
                             }]}>Moon</Text>
                         </View>
                     </View>
-                    
-                    <TextInput 
-                        style={[styles.input, {fontFamily: 'MitrRegular',}]} 
-                        theme={{ 
-                            roundness: 50, 
-                            colors: { onSurfaceVariant: 'grey'} ,
-                        }} 
+
+                    <TextInput
+                        style={[styles.input, { fontFamily: 'MitrRegular', }]}
+                        theme={{
+                            roundness: 50,
+                            colors: { onSurfaceVariant: 'grey' },
+                        }}
                         underlineColor="transparent"
                         activeUnderlineColor="grey"
                         textColor="black"
-    
+
                         label="ชื่อผู้ใช้"
                         onChangeText={(val) => this.inputValueUpdate(val, "username")}
                         value={this.state.username}
                     />
 
-                    <View style={{flexDirection: 'row'}}>
-                        <TextInput 
-                            style={styles.input} 
-                            theme={{ 
-                                roundness: 50, 
-                                colors: { onSurfaceVariant: 'grey'} 
-                            }} 
+                    <View style={{ flexDirection: 'row' }}>
+                        <TextInput
+                            style={styles.input}
+                            theme={{
+                                roundness: 50,
+                                colors: { onSurfaceVariant: 'grey' }
+                            }}
                             underlineColor="transparent"
                             activeUnderlineColor="grey"
                             textColor="black"
-                            secureTextEntry={this.state.showPassword} 
-        
+                            secureTextEntry={this.state.showPassword}
+
                             label="รหัสผ่าน"
                             onChangeText={(val) => this.inputValueUpdate(val, "password")}
                             value={this.state.password}
                         />
-                        <MaterialCommunityIcons 
-                            name={this.state.showPassword ? 'eye-off' : 'eye'} 
-                            size={24} 
+                        <MaterialCommunityIcons
+                            name={this.state.showPassword ? 'eye-off' : 'eye'}
+                            size={24}
                             color="#aaa"
-                            onPress={() => this.setState({ showPassword: !this.state.showPassword })} 
-                            style={{position:'absolute', right: 40, top: 30}}
+                            onPress={() => this.setState({ showPassword: !this.state.showPassword })}
+                            style={{ position: 'absolute', right: 40, top: 30 }}
                         />
                     </View>
-    
+
                     {/* <TouchableOpacity 
                         onPress={() => {
                             // navigation.navigate("tutorial", {});
@@ -139,14 +140,14 @@ class LoginScreen extends Component {
                             ลืมรหัสผ่าน
                         </Text>
                     </TouchableOpacity> */}
-    
+
                     <TouchableOpacity
                         style={styles.button}
                         onPress={() => {
                             this.state.all_data.map((item, i) => {
                                 if (this.state.username === item.username && this.state.password === item.password) {
                                     match = true
-                                    if(item.new_user == false) {
+                                    if (item.new_user == false) {
                                         this.props.navigation.navigate("homePage", {
                                             screen: "Profile",
                                             params: {
@@ -159,41 +160,50 @@ class LoginScreen extends Component {
                                             activeUser: item,
                                         });
                                     }
-                                    
+
                                     this.state.username = ""
                                     this.state.password = ""
                                     console.log('-- ActiveUser from Login : ', item);
-                                } 
-                            })  
+                                }
+                            })
                             if (!match) {
-                                alert('Invalid username or password')
-                            }                      
+                                Dialog.show({
+                                    type: ALERT_TYPE.WARNING,
+                                    title: (
+                                        <Text style={{ fontFamily: 'MitrRegular', fontSize: 18 }}>Invalid username or password</Text>
+                                    ),
+                                    button: 'OK',
+                                });
+                            }
                         }}
                     >
                         <Text style={styles.textButton}>เข้าสู่ระบบ</Text>
                     </TouchableOpacity>
 
                     {/* <HomeScreen user={this.state.activeUser} /> */}
-    
-                    <TouchableOpacity 
+
+                    <TouchableOpacity
                         onPress={() => {
                             this.props.navigation.navigate("register", {});
                         }}
                     >
-                        <Text style={[styles.text, { 
+                        <Text style={[styles.text, {
                             top: 15,
                             textShadowColor: 'rgba(0, 0, 0, 0.5)',
-                            textShadowOffset: {width: -1, height: 2},
+                            textShadowOffset: { width: -1, height: 2 },
                             textShadowRadius: 15,
                         }]}>
                             ไม่มีบัญชีผู้ใช้ ?{" "}
-                            <Text style={{ textDecorationLine: "underline"}}>
+                            <Text style={{ textDecorationLine: "underline" }}>
                                 สมัครสมาชิก
                             </Text>
                         </Text>
                     </TouchableOpacity>
-                        
+
                 </LinearGradient>
+                {/* เรียกใช้ alert */}
+                <AlertNotificationRoot>
+                </AlertNotificationRoot>
             </View>
         );
     }
@@ -204,7 +214,7 @@ async function cacheAssets() {
         { "MitrMedium": require("../assets/fonts/Mitr-Medium.ttf") },
         { "MitrRegular": require("../assets/fonts/Mitr-Regular.ttf") },
     ]);
-  
+
     await Promise.all([...fontAssets]);
 }
 
@@ -239,7 +249,7 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         paddingLeft: 5,
         fontFamily: 'MitrRegular',
-        
+
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -280,10 +290,10 @@ const styles = StyleSheet.create({
         elevation: 5,
     },
     textButton: {
-        color:"#FF9B80", 
+        color: "#FF9B80",
         fontSize: 20,
         fontFamily: 'MitrMedium',
     }
 });
-  
+
 export default LoginScreen;
