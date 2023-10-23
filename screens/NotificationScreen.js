9626201214c05873c4e4bdae1aaa816a6072258c
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-10-23 14:53:25 +0700

    ทำการแจ้งเตือน แต่ว่าเหลือที่ต้องกำหนดเวลา

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View, Image, Pressable, Switch } from 'react-native';
 // import ToggleSwitch from 'toggle-switch-react-native'
 // import { Button } from 'react-native-web';
+import * as Notifications from 'expo-notifications';
 
-import { useFonts } from 'expo-font'; 
+import { useFonts } from 'expo-font';
 
 const NotificationScreen = ({ route, navigation }) => {
+
+    // ตั้งเวลาแจ้งเตือน
+    const scheduleNotification = async (title, body, secondsFromNow) => {
+        try {
+            await Notifications.scheduleNotificationAsync({
+                content: {
+                    title: title,
+                    body: body,
+                    sound: 'default',
+                },
+                trigger: {
+                    seconds: secondsFromNow,
+                },
+            });
+        } catch (error) {
+            console.error('เกิดข้อผิดพลาดในการตั้งเวลาการแจ้งเตือน:', error);
+        }
+    };
+
+    // ตรวจสอบและขออนุญาตใช้การแจ้งเตือน
+    const askForNotificationPermission = async () => {
+        try {
+            const { status } = await Notifications.getPermissionsAsync();
+
+            if (status === 'granted') {
+                // ผู้ใช้มีสิทธิ์การแจ้งเตือนแล้ว
+                // จากที่นี้คุณสามารถเรียกใช้ `scheduleNotification`
+                scheduleNotification('คำเตือน', 'อย่าลืมพกผ้าอนามัยนะ', 10);
+                console.log('อย่าลืมพกผ้าอนามัยนะ')
+            } else {
+                // ผู้ใช้ไม่ได้รับสิทธิ์การแจ้งเตือน
+            }
+        } catch (error) {
+            console.error('เกิดข้อผิดพลาดในการขอสิทธิ์การแจ้งเตือน: askForNotificationPermission', error);
+        }
+    };
+
+
+    useEffect(() => {
+        // ขออนุญาตการแจ้งเตือนและตั้งเวลาการแจ้งเตือน
+        const setupNotification = async () => {
+            await askForNotificationPermission();
+        };
+        setupNotification();
+    }, []);
 
     const [loaded] = useFonts({
         MitrMedium: require('../assets/fonts/Mitr-Medium.ttf'),
@@ -16,78 +62,78 @@ const NotificationScreen = ({ route, navigation }) => {
         return null;
     }
 
-    return ( 
+    return (
         <View style={styles.screen}>
             <View style={styles.headerGroup}>
                 <Pressable onPress={() => {
-                            navigation.navigate("Profile", {});
-                        }}>
+                    navigation.navigate("Profile", {});
+                }}>
                     <Image
-                    source={require('../assets/profile/arrow-left.png')}
-                    style={styles.arrowleft}
+                        source={require('../assets/profile/arrow-left.png')}
+                        style={styles.arrowleft}
                     />
                 </Pressable>
                 <Text style={styles.header}>การแจ้งเตือน</Text>
             </View>
-            
+
             <Text style={styles.subHead}>
                 ทั่วไป
             </Text>
             <View style={styles.filterContainer}>
                 <Text style={styles.text}>การแจ้งเตือนทั้งหมด</Text>
                 <Switch
-                trackColor={{ true: "#9F79EB", false: "lightgray" }}
-                thumbColor={"blue"}
-                value={false}
+                    trackColor={{ true: "#9F79EB", false: "lightgray" }}
+                    thumbColor={"blue"}
+                    value={false}
                 />
             </View>
             <View style={styles.filterContainer}>
                 <Text style={styles.text}>รอบประจำเดือน</Text>
                 <Switch
-                trackColor={{ true: "#ff6f00", false: "lightgray" }}
-                thumbColor={"#ff6f00"}
-                value={false}
+                    trackColor={{ true: "#ff6f00", false: "lightgray" }}
+                    thumbColor={"#ff6f00"}
+                    value={false}
                 />
             </View>
             <View style={styles.filterContainer}>
                 <Text style={styles.text}>เสียง</Text>
                 <Switch
-                trackColor={{ true: "#ff6f00", false: "lightgray" }}
-                thumbColor={"#ff6f00"}
-                value={false}
+                    trackColor={{ true: "#ff6f00", false: "lightgray" }}
+                    thumbColor={"#ff6f00"}
+                    value={false}
                 />
             </View>
             <View style={styles.filterContainer}>
                 <Text style={styles.text}>การสั่น</Text>
                 <Switch
-                trackColor={{ true: "#ff6f00", false: "lightgray" }}
-                thumbColor={"#ff6f00"}
-                value={false}
+                    trackColor={{ true: "#ff6f00", false: "lightgray" }}
+                    thumbColor={"#ff6f00"}
+                    value={false}
                 />
             </View>
             <Text style={styles.subHead}>อัพเดตระบบและบริการ</Text>
             <View style={styles.filterContainer}>
                 <Text style={styles.text}>อัพเดตแอปพลิเคชัน</Text>
                 <Switch
-                trackColor={{ true: "#ff6f00", false: "lightgray" }}
-                thumbColor={"#ff6f00"}
-                value={false}
+                    trackColor={{ true: "#ff6f00", false: "lightgray" }}
+                    thumbColor={"#ff6f00"}
+                    value={false}
                 />
             </View>
             <View style={styles.filterContainer}>
                 <Text style={styles.text}>บทความใหม่</Text>
                 <Switch
-                trackColor={{ true: "#ff6f00", false: "lightgray" }}
-                thumbColor={"#ff6f00"}
-                value={false}
-            />
+                    trackColor={{ true: "#ff6f00", false: "lightgray" }}
+                    thumbColor={"#ff6f00"}
+                    value={false}
+                />
             </View>
 
-        {/* <Text style={styles.subHead}>
+            {/* <Text style={styles.subHead}>
             Others
         </Text> */}
-        
-        {/* <ToggleSwitch
+
+            {/* <ToggleSwitch
             isOn={false}
             onColor="#FC7D7B"
             offColor="#9F79EB"
